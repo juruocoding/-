58460dd55d3efef4a23ee56bb4b4f8bf675d774c
juruocoding <139749625+juruocoding@users.noreply.github.com>
2025-09-02 18:53:18 +0800

    Update Search_Get_Examine_Write.py
    精简代码

--- Search_Get_Examine_Write.py
+++ Search_Get_Examine_Write.py
@@ -3,44 +3,33 @@ import Write
 from bs4 import BeautifulSoup
 import tkinter as tk
 from tkinter import messagebox
-import re
-
-def examine(cnch,html,mode):
+import redef Examine(cnch,html,mode):
     soup = BeautifulSoup(html, "html.parser")
-    #print(soup.prettify())
+ 
     div=soup.find("div", class_="view_con clearfix")#得到的是一个节点
     notes=str(div)
     cnch=str(cnch)
-    #都得转字符串才行
     if mode==2 or mode==3:
         pattern = r';[\u4e00-\u9fff\uFF1B]+。'
         lst=re.findall(pattern,notes)
         info=""
         for match in lst:
-            info=info+match+"\n"
+            info=info+match.replace(";","->")+"\n"
         messagebox.showinfo(cnch,info)
     if mode==3:
         pass
     Write.write(cnch,notes)
 
-def search_get(cnch,text_utf8,mode):
+def Search_Get(cnch,text_utf8,mode):
     try:
         s_text=text_utf8.replace("\\","hw")
-        print(s_text)
         url="https://wyw.hwxnet.com/view/"+s_text[2:17]+".html"
-        print(url)
         r=requests.get(url)
-            
         r.raise_for_status()
         r.encoding = r.apparent_encoding
-        examine(cnch,r.text,mode)
+        Examine(cnch,r.text,mode)
     except:
         print("请求失败")
-    
-
-def Function(cnch,text_utf8,mode):  # 修复参数名称
-    search_get(cnch,text_utf8,mode)  # 修复参数名称
-    # messagebox.showinfo("提示", "搜索已完成")
+def Function(cnch,text_utf8,mode):  
+    Search_Get(cnch,text_utf8,mode)
     return
-# if __name__ == "__main__":
-#     print(Text)
